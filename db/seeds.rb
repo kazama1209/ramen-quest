25241a4ca16322dfacda38c199c5a8f205d10c15
kazama1209 <kazamanbou1209.pfm@gmail.com>
2019-12-07 19:03:01 +0000

    ready to deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  id: 1,
+  email: 'admin@gmail.com',
+  password: '12341234',
+  password_confirmation: '12341234',
+  role: :admin,
+  name: '管理人',
+  profile: 'よろしくお願いします。',
+  gender: 'female',
+  image: File.open("#{Rails.root}/db/fixtures/images/5.png"),
+)
+
+User.create!(
+  id: 2,
+  email: 'user1@gmail.com',
+  password: '12341234',
+  password_confirmation: '12341234',
+  name: '山田',
+  profile: 'よろしくお願いします。',
+  role: :user,
+  image: File.open("#{Rails.root}/db/fixtures/images/1.png"),
+)
+
+User.create!(
+  id: 3,
+  email: 'user2@gmail.com',
+  password: '12341234',
+  password_confirmation: '12341234',
+  role: :user,
+  name: '田中',
+  profile: 'よろしくお願いします。',
+  image: File.open("#{Rails.root}/db/fixtures/images/2.png"),
+)
+
+User.create!(
+  id: 4,
+  email: 'user3@gmail.com',
+  password: '12341234',
+  password_confirmation: '12341234',
+  role: :user,
+  name: '鈴木',
+  profile: 'よろしくお願いします。',
+  image: File.open("#{Rails.root}/db/fixtures/images/3.png"),
+)
+
+users = User.all
+users.each do |user|
+  user.activate!
+end
