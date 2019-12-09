14905a0a0427e54a8c99ab48da78cd89de21f680
kazama1209 <kazamanbou1209.pfm@gmail.com>
2019-12-09 12:29:24 +0000

    modify

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # ip_lookup: :maxmind,
 
   # to use an API key:
-  api_key: ENV['GMAP_API_KEY'],
+  api_key: "AIzaSyA5wnoZU4ZvnIszmKLdzAK1wn-9I-8QidU",
 
   # geocoding service request timeout, in seconds (default 3):
   timeout: 5,
