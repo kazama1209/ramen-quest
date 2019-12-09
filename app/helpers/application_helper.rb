d51cca923b59874eeb88aecbc8cfff7cf04767b2
kazama1209 <kazamanbou1209.pfm@gmail.com>
2019-12-09 14:17:43 +0000

    SEO

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: Settings.default_meta_tags.title,
+      charset: Settings.default_meta_tags.charset,
+      description: Settings.default_meta_tags.description,
+      keywords: Settings.default_meta_tags.keywords,
+      reverse: Settings.default_meta_tags.reverse,
+      og: {
+        title: Settings.default_meta_tags.og.title,
+        description: Settings.default_meta_tags.og.description,
+        type: Settings.default_meta_tags.og.type,
+        locale: Settings.default_meta_tags.og.locale,
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
